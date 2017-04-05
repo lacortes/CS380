ecf223058b830b66717d15edd9c40ff9913f9175
Luis Cortes <lacortes@cpp.edu>
2017-04-04 22:31:11 -0700

    EchoServer and EchoClient working

---
+++ EchoClient.java
@@ -0,0 +1,57 @@
+
+import java.io.InputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public final class EchoClient {
+
+    public static void main(String[] args) throws Exception {
+    	String info; // Store info from server
+    	Scanner kb = new Scanner(System.in);
+    	System.out.println("Connecting ... ");
+
+        try (Socket socket = new Socket("localhost", 2222)) {
+        	InputStream is = socket.getInputStream();
+        	InputStreamReader isr = new InputStreamReader(is, "UTF-8");
+			BufferedReader br = new BufferedReader(isr);
+        	System.out.println(br.readLine());
+           	
+           	// To print out to output stream. send to server
+           	PrintStream outStream = new PrintStream(socket.getOutputStream(), true, "UTF-8"); 
+            
+        	while(!socket.isClosed()) {
+            	System.out.print("Client> ");
+            	info = kb.nextLine();
+
+            	outStream.println(info); // Send to server
+
+            	info = br.readLine(); // read from server
+            	System.out.println("Server> "+info);
+            	
+            	if (info.equals("exit")) { 
+            		socket.close();
+            		continue;
+            	}
+        	}
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ EchoServer.java
@@ -0,0 +1,41 @@
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public final class EchoServer {
+
+    public static void main(String[] args) throws Exception {
+        try (ServerSocket serverSocket = new ServerSocket(2222)) {
+            while (true) {
+                try (Socket socket = serverSocket.accept()) {
+                    String address = socket.getInetAddress().getHostAddress();
+                    System.out.printf("Client connected: %s%n", address);
+
+                    OutputStream os = socket.getOutputStream();
+                    PrintStream out = new PrintStream(os, true, "UTF-8");
+                    out.printf("Hi %s, thanks for connecting!%n", address);
+                    
+                    InputStream in = socket.getInputStream();
+                    InputStreamReader inReader = new InputStreamReader(in, "UTF-8");
+                    BufferedReader buffReader = new BufferedReader(inReader);
+
+                    while (true) { // Poll Client until 'exit'
+                        String info = buffReader.readLine();
+                        // System.out.println(info);
+
+                        out.println(info);
+
+                        if (info.equals("exit")) {
+                            // System.out.println("EXITING");
+                            System.out.printf("Address %s is disconnected.\n", address);
+                            break;
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
