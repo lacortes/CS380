33425334d910978550d2040277151dc9edbf8f42
Luis Cortes <lacortes@cpp.edu>
2017-04-07 16:00:39 -0700

    Threading works

--- EchoServer.java
+++ EchoServer.java
@@ -14,36 +14,41 @@ public final class EchoServer {
     public static void main(String[] args) throws Exception {
         try (ServerSocket serverSocket = new ServerSocket(2222)) {
             while (true) {
-                try (Socket socket = serverSocket.accept()) {
-                    String address = socket.getInetAddress().getHostAddress();
-                    System.out.printf("Client connected: %s%n", address);
+                    Socket socket = serverSocket.accept();
 
-                    OutputStream os = socket.getOutputStream();
-                    PrintStream out = new PrintStream(os, true, "UTF-8");
-                    out.printf("Hi %s, thanks for connecting!%n", address);
-                    
-                    InputStream in = socket.getInputStream();
-                    InputStreamReader inReader = new InputStreamReader(in, "UTF-8");
-                    BufferedReader buffReader = new BufferedReader(inReader);
+                    Runnable r = () -> {
+                        try {
+                            String address = socket.getInetAddress().getHostAddress();
+                            System.out.printf("Client connected: %s%n", address);
 
-                    try {
-                        while (true) { // Poll Client until 'exit'
-                            String info = buffReader.readLine();
-                            // System.out.println(info);
+                            OutputStream os = socket.getOutputStream();
+                            PrintStream out = new PrintStream(os, true, "UTF-8");
+                            out.printf("Hi %s, thanks for connecting!%n", address);
+                            
+                            InputStream in = socket.getInputStream();
+                            InputStreamReader inReader = new InputStreamReader(in, "UTF-8");
+                            BufferedReader buffReader = new BufferedReader(inReader);
 
-                            out.println(info);
+                            try {
+                                while (true) { // Poll Client until 'exit'
+                                    String info = buffReader.readLine();
+                                    // System.out.println(info);
 
-                            if (info.equals("exit")) {
-                                // System.out.println("EXITING");
+                                    out.println(info);
+
+                                    if (info.equals("exit")) {
+                                        // System.out.println("EXITING");
+                                        System.out.printf("Address %s is disconnected.\n", address);
+                                        break;
+                                    }
+                                }
+                            } catch (NullPointerException e) {
                                 System.out.printf("Address %s is disconnected.\n", address);
-                                break;
                             }
-                        }
-                    } catch (NullPointerException e) {
-                        System.out.printf("Address %s is disconnected.\n", address);
-                    }
-                } 
+                        } catch (Exception e) {}
+                    };
+                    new Thread(r).start();
             }
-        }
+        } 
     }
 }
